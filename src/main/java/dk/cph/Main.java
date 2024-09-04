100f6331ea86497d707e2992ff9114dfdf563826
NikolajF <cph-nf70@cphbusiness.dk>
2024-09-04 11:53:15 +0200

    Added addStudent method in Course class, and prepped Main for adding objects

--- src/main/java/dk/cph/Main.java
+++ src/main/java/dk/cph/Main.java
@@ -2,9 +2,19 @@ package dk.cph;
 
 
 import dk.cph.config.HibernateConfig;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
 
 public class Main {
     public static void main(String[] args) {
-        HibernateConfig.getEntityManagerFactory(false);
+
+        EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory(false);
+
+        try(var em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+//            em.persist();
+            em.getTransaction().commit();
+        }
+
     }
 }
