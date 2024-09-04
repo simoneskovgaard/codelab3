0638988af7c3e122c44a3528a0cb440ea2aebc81
NikolajF <cph-nf70@cphbusiness.dk>
2024-09-04 12:07:44 +0200

    Methods added in StudentDaoImpl

--- src/main/java/dk/cph/dao/StudentDaoImpl.java
+++ src/main/java/dk/cph/dao/StudentDaoImpl.java
@@ -1,7 +1,9 @@
 package dk.cph.dao;
 
 import dk.cph.model.Student;
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.TypedQuery;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -23,26 +25,45 @@ public class StudentDaoImpl implements GenericDAO<Student, Integer> {
 
     @Override
     public List<Student> findAll() {
-        return List.of();
+        try (EntityManager em = emf.createEntityManager()) {
+            TypedQuery<Student> query = em.createQuery("select q from Student q", Student.class);
+            return query.getResultList();
+        }
     }
 
     @Override
     public void persistEntity(Student entity) {
-
+        try (EntityManager em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            em.persist(entity);
+            em.getTransaction().commit();
+        }
     }
 
     @Override
     public void removeEntity(Integer id) {
-
+        try (EntityManager em = emf.createEntityManager()) {
+            Student entity = em.find(Student.class, id);
+            em.getTransaction().begin();
+            em.remove(entity);
+            em.getTransaction().commit();
+        }
     }
 
     @Override
     public Student findEntity(Integer id) {
-        return null;
+        try (EntityManager em = emf.createEntityManager()) {
+            return em.find(Student.class, id);
+        }
     }
 
     @Override
     public Student updateEntity(Student entity, Integer id) {
-        return null;
+        try (EntityManager em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            Student updatedEntity = em.merge(entity);
+            em.getTransaction().commit();
+            return updatedEntity;
+        }
     }
 }
