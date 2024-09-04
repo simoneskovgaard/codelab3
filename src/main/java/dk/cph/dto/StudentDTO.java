a88fea7695c21d9aa10e4d6e325855bd82c2b89c
NikolajF <cph-nf70@cphbusiness.dk>
2024-09-04 11:39:01 +0200

    Added StudentDTO.

--- src/main/java/dk/cph/dto/StudentDTO.java
+++ src/main/java/dk/cph/dto/StudentDTO.java
@@ -1,4 +1,23 @@
 package dk.cph.dto;
 
+import dk.cph.model.Course;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
 public class StudentDTO {
+
+    private String name;
+    private String email;
+    private Set<Course> courses = new HashSet<>();
+
+    public StudentDTO(String name, String email, Set<Course> courses) {
+        this.name = name;
+        this.email = email;
+        this.courses = courses;
+    }
 }
