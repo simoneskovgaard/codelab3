a552d261c0af438e3702fdfb1ba70d84c5174031
Loudi <loudimail@gmail.com>
2024-09-04 12:42:15 +0200

    DTO and DTO sets

--- src/main/java/dk/cph/dto/CourseDTO.java
+++ src/main/java/dk/cph/dto/CourseDTO.java
@@ -1,4 +1,30 @@
 package dk.cph.dto;
 
+import dk.cph.model.Course;
+import dk.cph.model.CourseName;
+import dk.cph.model.Student;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
 public class CourseDTO {
+
+    private CourseName name;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private Set<StudentDTO> students;
+    private TeacherDTO teacher;
+
+    public CourseDTO(Course course) {
+        this.name = course.getCourseName();
+        this.startDate = course.getStartDate();
+        this.endDate = course.getEndDate();
+        for (Student student : course.getStudents()) {
+            students.add(new StudentDTO(student));
+        }
+        this.students = students;
+        this.teacher = new TeacherDTO(course.getTeacher());
+    }
+
 }
